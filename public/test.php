2affbd34b95496b3cd0c7092fdde90a04e766bc1
blue552 <22010697@st.phenikaa-uni.edu.vn>
2025-10-21 18:18:30 +0700

    Minimal setup to debug 500 error

---
+++ public/test.php
@@ -0,0 +1,7 @@
+<?php
+echo "PHP is working!";
+echo "<br>";
+echo "PHP Version: " . PHP_VERSION;
+echo "<br>";
+echo "Current time: " . date('Y-m-d H:i:s');
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hello from Railway! App is working.';
+    return 'Hello Railway!';
 });
 
 Route::get('/test', function () {
-    return response()->json(['status' => 'ok', 'message' => 'Test route working']);
-});
-
-Route::get('/health', function () {
-    return 'OK';
+    return 'Test OK';
 });
 
 // Temporarily commented out complex routes to debug 505 error
